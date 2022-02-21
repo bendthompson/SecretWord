2957378890cc217d042dd2ae3c92ca14f57225ee
ben thompson <ben97thompson@gmail.com>
2022-02-21 15:55:57 +0900

    added docstring

--- challenge4/lib.py
+++ challenge4/lib.py
@@ -1,4 +1,5 @@
 def try_me(secretword=''):
+    '''try different strings to guess the secret 3 letter word, hint, it's something on my desk'''
     if secretword == 'mug':
         return 'Success! Access granted'
     else:
